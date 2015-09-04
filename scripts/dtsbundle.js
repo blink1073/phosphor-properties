ed7a8c8b197e0b6dac13c8be2d1e5b5d3562bede
S. Chris Colbert <sccolbert@gmail.com>
2015-09-04 00:28:45 -0400

    update to TS 1.6

--- scripts/dtsbundle.js
+++
@@ -1,7 +0,0 @@
-require('dts-generator').generate({
-  name: 'phosphor-properties',
-  main: 'phosphor-properties/index',
-  baseDir: 'lib',
-  files: ['index.d.ts'],
-  out: 'lib/phosphor-properties.d.ts',
-});
